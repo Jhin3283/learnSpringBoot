3d07b8e8ff57ebe37cee8b109bcfd3363be3d24c
jhin3283 <hj3283@gmail.com>
2023-03-17 16:11:12 +0900

    ADD; repository and findAll Using JPA

--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/bean/Course.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/bean/Course.java
@@ -1,6 +1,5 @@
 package com.in28minutes.learnspringboot.courses.bean;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -10,16 +9,20 @@ public class Course {
 	@Id
 	@GeneratedValue
 	private long id;
-	
-	@Column
+
+//	@Column
 	private String name;
 	private String author;
-	
+
+	public Course() {
+	}
+
 	public Course(long id, String name, String author) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.author = author;
+
 	}
 
 	public long getId() {
@@ -38,5 +41,5 @@ public class Course {
 	public String toString() {
 		return "Course [id=" + id + ", name=" + name + ", author=" + author + "]";
 	}
-	
+
 }
--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -2,20 +2,25 @@ package com.in28minutes.learnspringboot.courses.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.learnspringboot.courses.bean.Course;
+import com.in28minutes.learnspringboot.courses.repository.CourseRepository;
 
 @RestController
 public class CourseController {
 
+	@Autowired
+	private CourseRepository repository;
+
 	@GetMapping("/courses")
 	public List<Course> getAllCourses() {
-		return List.of(new Course(1, "Learn Micro", "in28minutes"),
-				new Course(2, "Learn Full Stack", "in28minutes"));
+		return repository.findAll();
+//		return List.of(new Course(1, "Learn Micro", "in28minutes"), new Course(2, "Learn Full Stack", "in28minutes"));
 	}
-	
+
 	@GetMapping("/courses/2")
 	public Course getCourses() {
 		return new Course(2, "Learn Full Stack 1", "in28minutes");
